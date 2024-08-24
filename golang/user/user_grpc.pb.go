f2f517be80049dafa853d60a5d8e5096c429010d
nico-phil <nico-phil@users.noreply.github.com>
2024-08-24 14:41:44 +0000

    proto update

--- golang/user/user_grpc.pb.go
+++ golang/user/user_grpc.pb.go
@@ -19,139 +19,139 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Notification_Create_FullMethodName       = "/Notification/Create"
-	Notification_CreateDevice_FullMethodName = "/Notification/CreateDevice"
+	User_Create_FullMethodName       = "/User/Create"
+	User_CreateDevice_FullMethodName = "/User/CreateDevice"
 )
 
-// NotificationClient is the client API for Notification service.
+// UserClient is the client API for User service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type NotificationClient interface {
+type UserClient interface {
 	Create(ctx context.Context, in *CreateUserRequest, opts ...grpc.CallOption) (*CreateUserResponse, error)
 	CreateDevice(ctx context.Context, in *CreateDeviceRequest, opts ...grpc.CallOption) (*CreateDeviceResponse, error)
 }
 
-type notificationClient struct {
+type userClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewNotificationClient(cc grpc.ClientConnInterface) NotificationClient {
-	return &notificationClient{cc}
+func NewUserClient(cc grpc.ClientConnInterface) UserClient {
+	return &userClient{cc}
 }
 
-func (c *notificationClient) Create(ctx context.Context, in *CreateUserRequest, opts ...grpc.CallOption) (*CreateUserResponse, error) {
+func (c *userClient) Create(ctx context.Context, in *CreateUserRequest, opts ...grpc.CallOption) (*CreateUserResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(CreateUserResponse)
-	err := c.cc.Invoke(ctx, Notification_Create_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, User_Create_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *notificationClient) CreateDevice(ctx context.Context, in *CreateDeviceRequest, opts ...grpc.CallOption) (*CreateDeviceResponse, error) {
+func (c *userClient) CreateDevice(ctx context.Context, in *CreateDeviceRequest, opts ...grpc.CallOption) (*CreateDeviceResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(CreateDeviceResponse)
-	err := c.cc.Invoke(ctx, Notification_CreateDevice_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, User_CreateDevice_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// NotificationServer is the server API for Notification service.
-// All implementations must embed UnimplementedNotificationServer
+// UserServer is the server API for User service.
+// All implementations must embed UnimplementedUserServer
 // for forward compatibility.
-type NotificationServer interface {
+type UserServer interface {
 	Create(context.Context, *CreateUserRequest) (*CreateUserResponse, error)
 	CreateDevice(context.Context, *CreateDeviceRequest) (*CreateDeviceResponse, error)
-	mustEmbedUnimplementedNotificationServer()
+	mustEmbedUnimplementedUserServer()
 }
 
-// UnimplementedNotificationServer must be embedded to have
+// UnimplementedUserServer must be embedded to have
 // forward compatible implementations.
 //
 // NOTE: this should be embedded by value instead of pointer to avoid a nil
 // pointer dereference when methods are called.
-type UnimplementedNotificationServer struct{}
+type UnimplementedUserServer struct{}
 
-func (UnimplementedNotificationServer) Create(context.Context, *CreateUserRequest) (*CreateUserResponse, error) {
+func (UnimplementedUserServer) Create(context.Context, *CreateUserRequest) (*CreateUserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
 }
-func (UnimplementedNotificationServer) CreateDevice(context.Context, *CreateDeviceRequest) (*CreateDeviceResponse, error) {
+func (UnimplementedUserServer) CreateDevice(context.Context, *CreateDeviceRequest) (*CreateDeviceResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateDevice not implemented")
 }
-func (UnimplementedNotificationServer) mustEmbedUnimplementedNotificationServer() {}
-func (UnimplementedNotificationServer) testEmbeddedByValue()                      {}
+func (UnimplementedUserServer) mustEmbedUnimplementedUserServer() {}
+func (UnimplementedUserServer) testEmbeddedByValue()              {}
 
-// UnsafeNotificationServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to NotificationServer will
+// UnsafeUserServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to UserServer will
 // result in compilation errors.
-type UnsafeNotificationServer interface {
-	mustEmbedUnimplementedNotificationServer()
+type UnsafeUserServer interface {
+	mustEmbedUnimplementedUserServer()
 }
 
-func RegisterNotificationServer(s grpc.ServiceRegistrar, srv NotificationServer) {
-	// If the following call pancis, it indicates UnimplementedNotificationServer was
+func RegisterUserServer(s grpc.ServiceRegistrar, srv UserServer) {
+	// If the following call pancis, it indicates UnimplementedUserServer was
 	// embedded by pointer and is nil.  This will cause panics if an
 	// unimplemented method is ever invoked, so we test this at initialization
 	// time to prevent it from happening at runtime later due to I/O.
 	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
 		t.testEmbeddedByValue()
 	}
-	s.RegisterService(&Notification_ServiceDesc, srv)
+	s.RegisterService(&User_ServiceDesc, srv)
 }
 
-func _Notification_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _User_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateUserRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(NotificationServer).Create(ctx, in)
+		return srv.(UserServer).Create(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Notification_Create_FullMethodName,
+		FullMethod: User_Create_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(NotificationServer).Create(ctx, req.(*CreateUserRequest))
+		return srv.(UserServer).Create(ctx, req.(*CreateUserRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Notification_CreateDevice_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _User_CreateDevice_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateDeviceRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(NotificationServer).CreateDevice(ctx, in)
+		return srv.(UserServer).CreateDevice(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Notification_CreateDevice_FullMethodName,
+		FullMethod: User_CreateDevice_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(NotificationServer).CreateDevice(ctx, req.(*CreateDeviceRequest))
+		return srv.(UserServer).CreateDevice(ctx, req.(*CreateDeviceRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Notification_ServiceDesc is the grpc.ServiceDesc for Notification service.
+// User_ServiceDesc is the grpc.ServiceDesc for User service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Notification_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "Notification",
-	HandlerType: (*NotificationServer)(nil),
+var User_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "User",
+	HandlerType: (*UserServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Create",
-			Handler:    _Notification_Create_Handler,
+			Handler:    _User_Create_Handler,
 		},
 		{
 			MethodName: "CreateDevice",
-			Handler:    _Notification_CreateDevice_Handler,
+			Handler:    _User_CreateDevice_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
